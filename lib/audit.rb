fd3a3edbf2b91461d1c6d02d40603d88b73ea2f1
Laney McGlohon <laneymcg@stanford.edu>
2016-10-13 12:57:12 -0700

    Add comparison methods

--- lib/audit.rb
+++ lib/audit.rb
@@ -52,6 +52,7 @@ def druids_from_results(id_array)
   druids
 end
 
+# https://sul-solr.stanford.edu/solr/argo3_prod/select?&fq=is_member_of_collection_ssim:%22info:fedora/druid:$druid%22&fl=id&rows=1000&sort=id%20asc&wt=csv&csv.header=false
 def coll_members_from_argo(collection_ids)
   members = []
   collection_ids.each do | druid |
@@ -60,96 +61,111 @@ def coll_members_from_argo(collection_ids)
   members
 end
 
+# https://sul-solr.stanford.edu/solr/purl-prod/select?&fq=is_member_of_collection_ssim:"druid:$druid"&fl=id&rows=1000&sort=id%20asc&wt=csv&csv.header=false
+def coll_members_from_purl_fetcher
+end
+
+# https://sul-solr.stanford.edu/solr/sw-preview-stage/select?&fq=collection:$druid&fl=id&rows=1000&sort=id%20asc&wt=csv&csv.header=false
+def coll_members_from_SearchWorks
+end
+
 def no_pages(data)
   data["pages"]["total_pages"]
 end
 
-# Need to compare druids from argo and from sw-prod index
-# Get druid, released_to array, and catkey in comma separated results
-# Resulting Argo list is sorted by druid
-argo_coll_results = results("https://sul-solr.stanford.edu/solr/argo3_prod/select?&fq=objectType_ssim:%22collection%22&fl=id,released_to_ssim,catkey_id_ssim&rows=10000&sort=id%20asc&wt=csv&csv.header=false")
-argo_released_druids = results("https://sul-solr-a.stanford.edu/solr/argo3_prod/select?fl=id,released_to_ssim,catkey_id_ssim&fq=released_to_ssim:*&q=*:*&rows=1000000&wt=csv")
-
-
-# Only care about druids released to Searchworks
-released = []
-argo_druids = []
-argo_coll_results.each do | res |
-  pieces = res.split(',')
-  # Look for searchworks in the second column for each result
-  # and remove druid: prefix on druid in first column
-  if (!pieces[1].nil? && pieces[1].downcase == "searchworks")
-    pieces[0].gsub! "druid:", ""
-    released.push(pieces)
-    argo_druids.push(pieces[0])
-  end
-end
-
-# Get all collection druids that have been released to SearchWorks production
-coll = JSON.parse(results("https://purl-fetcher-prod.stanford.edu/collections"))
-
-coll_ids = []
-coll_ids += ids_from_purl_fetcher(coll["collections"])
-
-(2..no_pages(coll)).each do |i|
-  coll = JSON.parse(results("https://purl-fetcher-prod.stanford.edu/collections?page=#{i}"))
-  coll_ids += ids_from_purl_fetcher(coll["collections"])
-end
-
-coll_druids = druids_from_results(coll_ids)
-
-# Get all druids are released to SearchWorks production
-purl = JSON.parse(results("https://purl-fetcher.stanford.edu/purls?target=SearchWorks&per_page=10000"))
-
-purl_ids = []
-purl_ids += ids_from_purl_fetcher(purl["purls"])
-
-(2..no_pages(purl)).each do |i|
-  puts i
-  purl = JSON.parse(results("https://purl-fetcher-prod.stanford.edu/purls?target=SearchWorks&page=#{i}&per_page=10000"))
-  purl_ids += ids_from_purl_fetcher(purl["purls"])
-end
-
-purl_druids = druids_from_results(purl_ids)
-
-# Get all IDs that are druids and all druids in the managed_purl_urls fields
-lb_results = results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A%2F%5Ba-z%5D%7B2%7D%5B0-9%5D%7B3%7D%5Ba-z%5D%7B2%7D%5B0-9%5D%7B4%7D%2F&fl=id,managed_purl_urls&wt=csv&rows=10000000&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A1*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A2*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A3*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A4*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A5*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A6*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A7*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A8*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
-             results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A9*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false")
-
-# To get members of collections
-# https://sul-solr.stanford.edu/solr/argo3_prod/select?&fq=is_member_of_collection_ssim:%22info:fedora/druid:$druid%22&fl=id&rows=1000&sort=id%20asc&wt=csv&csv.header=false
-# https://sul-solr.stanford.edu/solr/purl-prod/select?&fq=is_member_of_collection_ssim:"druid:$druid"&fl=id&rows=1000&sort=id%20asc&wt=csv&csv.header=false
-# https://sul-solr.stanford.edu/solr/sw-preview-stage/select?&fq=collection:$druid&fl=id&rows=1000&sort=id%20asc&wt=csv&csv.header=false
-
-sw_lb_druids = []
-lb_results.each do | l |
-  out = l.split(",")
-  out.each do | o |
-    if (o =~ /^[a-z]/)
-      o.gsub!("\"", "")
-      o.gsub!("http:\/\/purl.stanford.edu\/", "")
-      sw_lb_druids.push(o)
+def druids_from_SearchWorks(results)
+  druids = []
+  results.each do | res |
+    urls = res.split(",")
+    urls.each do | url |
+      if (url =~ /^[a-z]/)
+        url.gsub!("\"", "")
+        url.gsub!("http:\/\/purl.stanford.edu\/", "")
+        druids.push(url)
+      end
     end
   end
+  druids
 end
 
-sw_lb_druids = sw_lb_druids.uniq
+def individual_items_in_argo_released_to_SearchWorks_prod(argo_ind_items)
+  # Only care about druids released to Searchworks
+  druids = []
+  argo_ind_items.each do | res |
+    pieces = res.split(',')
+    # Look for searchworks in the second column for each result
+    # and remove druid: prefix on druid in first column
+    if (!pieces[1].nil? && pieces[1].downcase == "searchworks")
+      pieces[0].gsub! "druid:", ""
+      druids.push(pieces[0])
+    end
+  end
+  druids
+end
 
-# compare results
-# argo_results format is druid, searchworks, catkeys
-# purl_fetcher results format is druid, catkeys
-# searchworks loadbalancer results from searching for druids as an ID and records with purl.stanford.edu in them
-# the format is druid or catkey, managed_purl_urls as an array
+def compare_collections
+  # Argo collection druids
+  argo_coll_results = results("https://sul-solr.stanford.edu/solr/argo3_prod/select?&fq=objectType_ssim:%22collection%22&fl=id,released_to_ssim,catkey_id_ssim&rows=10000&sort=id%20asc&wt=csv&csv.header=false")
 
-# Get the differences in the druids from argo and purl-fetcher-prod
+  # Purl_fetcher collection druids
+  coll = JSON.parse(results("https://purl-fetcher-prod.stanford.edu/collections"))
+
+  coll_ids = []
+  coll_ids += ids_from_purl_fetcher(coll["collections"])
+
+  (2..no_pages(coll)).each do |i|
+    coll = JSON.parse(results("https://purl-fetcher-prod.stanford.edu/collections?page=#{i}"))
+    coll_ids += ids_from_purl_fetcher(coll["collections"])
+  end
+
+  coll_druids = druids_from_results(coll_ids)
+
+  #SearchWorks production collection druids
+  lb_results = results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=collection_type%3A%22Digital+Collection%22&rows=1000&fl=id&wt=csv&csv.header=false")
+end
+
+def compare_collection_members
+end
+
+def compare_individual_items
+
+  # Argo individual item druids that are released
+  argo_released_druids = results("https://sul-solr-a.stanford.edu/solr/argo3_prod/select?fl=id,released_to_ssim,catkey_id_ssim&fq=released_to_ssim:*&q=*:*&rows=1000000&wt=csv")
+
+  # Argo druids released to SearchWorks production
+  argo_druids = individual_items_in_argo_released_to_SearchWorks_prod(argo_ind_items)
+
+  # All Purl_fetcher druids that are released to SearchWorks production
+  purl = JSON.parse(results("https://purl-fetcher.stanford.edu/purls?target=SearchWorks&per_page=10000"))
+
+  purl_ids = []
+  purl_ids += ids_from_purl_fetcher(purl["purls"])
+
+  (2..no_pages(purl)).each do |i|
+    puts i
+    purl = JSON.parse(results("https://purl-fetcher-prod.stanford.edu/purls?target=SearchWorks&page=#{i}&per_page=10000"))
+    purl_ids += ids_from_purl_fetcher(purl["purls"])
+  end
+
+  purl_druids = druids_from_results(purl_ids)
+
+  # Get all SearchWorks IDs that are druids and all druids in the
+  # managed_purl_urls fields
+  lb_results = results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A%2F%5Ba-z%5D%7B2%7D%5B0-9%5D%7B3%7D%5Ba-z%5D%7B2%7D%5B0-9%5D%7B4%7D%2F&fl=id,managed_purl_urls&wt=csv&rows=10000000&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A1*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A2*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A3*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A4*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A5*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A6*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A7*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A8*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false") +
+               results("http://searchworks-solr-lb:8983/solr/current/select?q=*%3A*&fq=id%3A9*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false")
+
+  sw_lb_druids = druids_from_managed_purls(lb_results)
+
+end
+
 argo_druids_not_purl = argo_druids - purl_druids
 purl_druids_not_argo = purl_druids - argo_druids
 # argo_druids_not_sw_lb =
