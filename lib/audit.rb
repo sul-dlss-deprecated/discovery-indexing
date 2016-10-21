6e47e4ecfb6d64f0214a3aa4502c39ff563c6f2e
Laney McGlohon <laneymcg@stanford.edu>
2016-10-21 13:41:36 -0700

    Added environment variables, cleaned up the sw query, and allowed for designation of searchworks and searchworkspreview

--- lib/audit.rb
+++ lib/audit.rb
@@ -17,17 +17,23 @@ require 'json'
 require 'net/http'
 require 'uri'
 
+# Environment variables
+argo_url = ENV['ARGO_URL']
+pf_url = ENV['PF_URL']
+sw_url = ENV['SW_URL']
+sw_target = ENV['SW_TGT']
+
 def results(url)
   res_url = URI.parse(url)
   Net::HTTP.get_response(res_url).body
 end
 
-def ids_from_purl_fetcher(data)
+def ids_from_purl_fetcher(data, tgt)
   data_ids = []
   data.each do | d |
     if !d["true_targets"].nil? && !d["true_targets"].empty?
       d["true_targets"].map!(&:downcase)
-      if d["true_targets"].include? "searchworks"
+      if d["true_targets"].include? tgt
         if d["catkey"].nil? || d["catkey"].empty?
           data_ids.push(d["druid"].gsub(/druid:/, ''))
         else
@@ -78,53 +84,52 @@ def druids_from_SearchWorks(results)
   druids
 end
 
-def argo_druids(argo_url)
+def argo_druids(url, tgt)
   # Argo collection druids
   query = "/select?&fq=objectType_ssim:%22collection%22&fl=id,released_to_ssim,catkey_id_ssim&rows=10000&sort=id%20asc&wt=csv&csv.header=false"
-  argo_coll_results = results(argo_url + query).split("\n")
-  individual_items_in_argo_released_to_SearchWorks_prod(argo_coll_results)
+  argo_coll_results = results("#{url + query}").split("\n")
+  individual_items_released_in_argo_to_tgt(argo_coll_results, tgt)
 end
 
-def pf_druids(pf_url)
+def pf_druids(url, tgt)
   # Purl_fetcher collection druids
   query = "/collections"
-  coll = JSON.parse(results(pf_url + query))
+  coll = JSON.parse(results("#{url + query}"))
 
   coll_ids = []
-  coll_ids += ids_from_purl_fetcher(coll["collections"])
+  coll_ids += ids_from_purl_fetcher(coll["collections"], tgt)
 
   (2..no_pages(coll)).each do |i|
-    coll = JSON.parse(results("#{pf_url + query}?page=#{i}"))
-    coll_ids += ids_from_purl_fetcher(coll["collections"])
+    coll = JSON.parse(results("#{url + query}?page=#{i}"))
+    coll_ids += ids_from_purl_fetcher(coll["collections"], tgt)
   end
 
   druids_from_results(coll_ids)
 end
 
-def sw_druids(sw_url)
+# Get sw records that have druids as IDs but are not collections
+def records_with_druid_ids(url)
+  results("#{sw_url}/select?fq=-collection_type%3A%22Digital+Collection%22&q=*%3A*&fq=id%3A%2F%5Ba-z%5D%7B2%7D%5B0-9%5D%7B3%7D%5Ba-z%5D%7B2%7D%5B0-9%5D%7B4%7D%2F&fl=id,managed_purl_urls&wt=csv&rows=10000000&csv.header=false").split("\n")
+end
+
+def sw_druids(url)
   #SearchWorks production collection druids
-  query = "/select?q=*%3A*&fq=collection_type%3A%22Digital+Collection%22&rows=1000&fl=id&wt=csv&csv.header=false"
-  lb_results = results("#{sw_url + query}?q=*%3A*&fq=id%3A%2F%5Ba-z%5D%7B2%7D%5B0-9%5D%7B3%7D%5Ba-z%5D%7B2%7D%5B0-9%5D%7B4%7D%2F&fl=id,managed_purl_urls&wt=csv&rows=10000000&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A1*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A2*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A3*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A4*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A5*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A6*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A7*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A8*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n") +
-               results("#{sw_url + query}?q=*%3A*&fq=id%3A9*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false").split("\n")
+  query = "/select?&fq=collection_type%3A%22Digital+Collection%22&q=*%3A*&rows=10000000&fl=id%2Cmanaged_purl_urls&wt=csv&csv.header=false"
+  # This first results statement finds all Digital Collections records with druids as ids (id:/[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}/))
+  # the rest of the results statements look for records with catkeys, ie ids that start with a number
+  id_array = ["%2F%5Ba-z%5D%7B2%7D%5B0-9%5D%7B3%7D%5Ba-z%5D%7B2%7D%5B0-9%5D%7B4%7D%2F"] +  (1..9).map { |v| "#{v}*" }
+  lb_results = id_array.map { |id| results("#{url + query}&fq=id%3A#{id}").split("\n")}.flatten
   druids_from_SearchWorks(lb_results).uniq.sort
 end
 
-def individual_items_in_argo_released_to_SearchWorks_prod(argo_ind_items)
-  # Only care about druids released to Searchworks
+def individual_items_released_in_argo_to_tgt(argo_ind_items, tgt)
+  # Only care about druids released to specified tgt
   druids = []
   argo_ind_items.each do | res |
     pieces = res.split(',')
     # Look for searchworks in the second column for each result
     # and remove druid: prefix on druid in first column
-    if (!pieces[1].nil? && pieces[1].downcase == "searchworks")
+    if ((!pieces[1].nil? && pieces[1].downcase == tgt) || tgt == "searchworkspreview")
       pieces[0].gsub! "druid:", ""
       druids.push(pieces[0])
     end
@@ -132,13 +137,14 @@ def individual_items_in_argo_released_to_SearchWorks_prod(argo_ind_items)
   druids
 end
 
-argo_res = argo_druids(ENV['ARGO_URL'])
-pf_res = pf_druids(ENV['PF_URL'])
-sw_res = sw_druids(ENV['SW_URL'])
+argo_res = argo_druids(argo_url, sw_target)
+pf_res = pf_druids(pf_url, sw_target)
+sw_res = sw_druids(sw_url)
+
 puts("Collections Statistics")
-puts("Argo Production has #{argo_res.length} released to Searchworks")
-puts("PF Production has #{pf_res.length} released to Searchworks")
-puts("SW Production has #{sw_res.length} released in Searchworks")
+puts("Argo has #{argo_res.length} released to #{sw_target}")
+puts("PF has #{pf_res.length} released to #{sw_target}")
+puts("SW has #{sw_res.length} released in #{sw_target}")
 
 puts("These druids are in Argo as released but not in PF")
 puts argo_res.sort - pf_res.sort
