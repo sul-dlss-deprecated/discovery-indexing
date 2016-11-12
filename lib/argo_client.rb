6046d1b3e8236fd81432e1e7c79bf760c1bf27d8
Laney McGlohon <laneymcg@stanford.edu>
2016-11-12 05:26:43 -0800

    Update queries

--- lib/argo_client.rb
+++ lib/argo_client.rb
@@ -10,33 +10,32 @@ class ArgoClient
     @tgt = tgt.downcase
   end
 
-  def results(url_with_params)
-    res_url = URI.parse(url_with_params)
-    Net::HTTP.get_response(res_url).body
+  def results(query)
+    res_url = URI.parse("#{url + query}")
+    resp = JSON.parse(Net::HTTP.get_response(res_url).body)
+    individual_items_released_to_tgt(resp["response"]["docs"])
   end
 
   def collections_druids
     # Argo collection druids
-    # Query is fq=objectType_ssim:"collection"
+    # Query is fq=objectType_ssim:"collection", fq=released_to_ssim:*
     # number of rows to output is 10000
-    # output fields are id, released_to_ssim, and catkey_id_ssim
+    # output fields are id, released_to_ssim, catkey_id_ssim, processing_status_text_ssi, rights_descriptions_ssim
     # output ascending sorted by id
     # output format is json
-    query = "/select?&fq=objectType_ssim%3A%22collection%22&fl=id,released_to_ssim,catkey_id_ssim&rows=10000&sort=id%20asc&wt=json"
-    argo_coll_results = JSON.parse(results("#{url + query}"))
-    individual_items_released_to_tgt(argo_coll_results["response"]["docs"])
+    query = "/select?&fq=objectType_ssim%3A%22collection%22&fq=released_to_ssim%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=10000&sort=id%20asc&wt=json"
+    results(query)
   end
 
   def items_druids
     # Argo item druids released but not in a collection
     # Query is fq=-is_member_of_collection_ssim:*, fq=objectType_ssim:"item", fq=released_to_ssim:*
     # number of rows to output is 10000
-    # output fields are id, released_to_ssim, and catkey_id_ssim
+    # output fields are id, released_to_ssim, catkey_id_ssim, processing_status_text_ssi, rights_descriptions_ssim
     # output ascending sorted by id
     # output format is json
     query = "/select?fq=-is_member_of_collection_ssim%3A*&fq=objectType_ssim%3A%22item%22&fq=released_to_ssim%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=10000&sort=id%20asc&wt=json"
-    argo_coll_results = JSON.parse(results("#{url + query}"))
-    individual_items_released_to_tgt(argo_coll_results["response"]["docs"])
+    results(query)
   end
 
   def all_druids
@@ -46,9 +45,8 @@ class ArgoClient
     # output fields are id, released_to_ssim, and catkey_id_ssim
     # output ascending sorted by id
     # output format is json
-    query = "/select?&fq=released_to_ssim%3A*&q=*%3A*&fl=id,released_to_ssim,catkey_id_ssim&rows=1000000&sort=id%20asc&wt=json"
-    argo_all_results = JSON.parse(results("#{url + query}"))
-    individual_items_released_to_tgt(argo_all_results["response"]["docs"])
+    query = "/select?&fq=released_to_ssim%3A*&q=*%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=1000000&sort=id%20asc&wt=json"
+    results(query)
   end
 
   def individual_items_released_to_tgt(argo_ind_items)
@@ -56,7 +54,7 @@ class ArgoClient
     # rights anything but dark
     druids = []
     argo_ind_items.each do | res |
-      if res.has_key?("released_to_ssim") && res["processing_status_text_ssi"] != "Registered" && !res["rights_descriptions_ssim"].include?("dark")
+      if res["processing_status_text_ssi"] != "Registered" && !res["rights_descriptions_ssim"].include?("dark")
         res["released_to_ssim"].map!(&:downcase)
         uniq_tgt = res["released_to_ssim"].uniq
         if uniq_tgt.include? tgt
@@ -69,14 +67,13 @@ class ArgoClient
 
   def collection_members(coll_druid)
     # coll_members_from_argo
-    # Query is fq=is_member_of_collection_ssim:"info:fedora/druid:#{coll_druid}"
+    # Query is fq=is_member_of_collection_ssim:"info:fedora/druid:#{coll_druid}", fq=released_to_ssim:*
     # number of rows to output is 1000000
-    # output fields are id, released_to_ssim, and catkey_id_ssim
+    # output fields are id, released_to_ssim, catkey_id_ssim, processing_status_text_ssi, rights_descriptions_ssim
     # output ascending sorted by id
     # output format is json
-    query = "/select?&fq=is_member_of_collection_ssim%3A%22info:fedora/druid:#{coll_druid}%22&fl=id,released_to_ssim,catkey_id_ssim&rows=1000000&sort=id%20asc&wt=json"
-    argo_mem_results = JSON.parse(results("#{url + query}"))
-    individual_items_released_to_tgt(argo_mem_results["response"]["docs"])
+    query = "/select?&fq=is_member_of_collection_ssim%3A%22info%3Afedora/druid%3A#{coll_druid}%22&fq=released_to_ssim%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=1000000&sort=id%20asc&wt=json"
+    results(query)
   end
 
 end
