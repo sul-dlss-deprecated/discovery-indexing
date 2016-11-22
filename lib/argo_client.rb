aa398cce263d3d2de567c7bdb9b71be52d7a61ca
Laney McGlohon <laneymcg@stanford.edu>
2016-11-22 10:47:58 -0800

    Add collection information determination from argo for use in the output

--- lib/argo_client.rb
+++ lib/argo_client.rb
@@ -12,8 +12,18 @@ class ArgoClient
 
   def results(query)
     res_url = URI.parse("#{url + query}")
-    resp = JSON.parse(Net::HTTP.get_response(res_url).body)
-    individual_items_released_to_tgt(resp["response"]["docs"])
+    JSON.parse(Net::HTTP.get_response(res_url).body)
+  end
+
+  def collections_info
+    # Argo collection information
+    # Query is fq=objectType_ssim:"collection", fq=released_to_ssim:*
+    # number of rows to output is 10000
+    # output fields are id, catkey_id_ssim, and title_info_0_main_title_ssim
+    # output ascending sorted by id
+    # output format is json
+    query = "/select?&fq=objectType_ssim%3A%22collection%22&fl=id,catkey_id_ssim,title_info_0_main_title_ssim&rows=10000&sort=id%20asc&wt=json"
+    results(query)
   end
 
   def collections_druids
@@ -24,7 +34,7 @@ class ArgoClient
     # output ascending sorted by id
     # output format is json
     query = "/select?&fq=objectType_ssim%3A%22collection%22&fq=released_to_ssim%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=10000&sort=id%20asc&wt=json"
-    results(query)
+    individual_items_released_to_tgt(results(query))
   end
 
   def items_druids
@@ -35,7 +45,7 @@ class ArgoClient
     # output ascending sorted by id
     # output format is json
     query = "/select?fq=-is_member_of_collection_ssim%3A*&fq=objectType_ssim%3A%22item%22&fq=released_to_ssim%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=10000&sort=id%20asc&wt=json"
-    results(query)
+    individual_items_released_to_tgt(results(query))
   end
 
   def all_druids
@@ -46,14 +56,25 @@ class ArgoClient
     # output ascending sorted by id
     # output format is json
     query = "/select?&fq=released_to_ssim%3A*&q=*%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=1000000&sort=id%20asc&wt=json"
-    results(query)
+    individual_items_released_to_tgt(results(query))
+  end
+
+  def collection_members(coll_druid)
+    # coll_members_from_argo
+    # Query is fq=is_member_of_collection_ssim:"info:fedora/druid:#{coll_druid}", fq=released_to_ssim:*
+    # number of rows to output is 1000000
+    # output fields are id, released_to_ssim, catkey_id_ssim, processing_status_text_ssi, rights_descriptions_ssim
+    # output ascending sorted by id
+    # output format is json
+    query = "/select?&fq=is_member_of_collection_ssim%3A%22info%3Afedora/druid%3A#{coll_druid}%22&fq=released_to_ssim%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=1000000&sort=id%20asc&wt=json"
+    individual_items_released_to_tgt(results(query))
   end
 
   def individual_items_released_to_tgt(argo_ind_items)
     # Only care about druids released to specified tgt with processing status anything but Registered and
     # rights anything but dark
     druids = []
-    argo_ind_items.each do | res |
+    argo_ind_items["response"]["docs"].each do | res |
       if res["processing_status_text_ssi"] != "Registered" && !res["rights_descriptions_ssim"].include?("dark")
         res["released_to_ssim"].map!(&:downcase)
         uniq_tgt = res["released_to_ssim"].uniq
@@ -65,15 +86,13 @@ class ArgoClient
     druids.uniq.sort
   end
 
-  def collection_members(coll_druid)
-    # coll_members_from_argo
-    # Query is fq=is_member_of_collection_ssim:"info:fedora/druid:#{coll_druid}", fq=released_to_ssim:*
-    # number of rows to output is 1000000
-    # output fields are id, released_to_ssim, catkey_id_ssim, processing_status_text_ssi, rights_descriptions_ssim
-    # output ascending sorted by id
-    # output format is json
-    query = "/select?&fq=is_member_of_collection_ssim%3A%22info%3Afedora/druid%3A#{coll_druid}%22&fq=released_to_ssim%3A*&fl=id,released_to_ssim,catkey_id_ssim,processing_status_text_ssi,rights_descriptions_ssim&rows=1000000&sort=id%20asc&wt=json"
-    results(query)
+  def coll_info(colls)
+    coll_data = {}
+    colls["response"]["docs"].each do | res |
+      res["id"].gsub!("druid:", "")
+      coll_data[res["id"]] = { "catkey" => res["catkey_id_ssim"],"title" => res["title_info_0_main_title_ssim"]}
+    end
+    coll_data
   end
 
 end
--- lib/audit.rb
+++ lib/audit.rb
@@ -33,6 +33,7 @@ collection_druid = ENV['COLL_DRUID']
 argo_client = ArgoClient.new(argo_url, sw_target)
 purl_client = PurlClient.new(pf_url, sw_target)
 sw_client = SwClient.new(sw_url)
+coll_data = argo_client.coll_info(argo_client.collections_info)
 
 def differences(ar, pf, sw)
   diff = {}
@@ -56,21 +57,23 @@ def system(s)
   end
 end
 
-def rpt_output(com, sw_target)
+def rpt_output(com, coll_data, sw_target)
   com.keys.each do |c|
     sys = c.split("_")
     first = system(sys[0])
     second = system(sys[1])
     if (com[c].length > 0)
       puts("These druids are in #{first} as released to #{sw_target} but not in #{second}")
-      puts com[c]
+      com[c].each do |ele|
+        puts "#{ele} #{coll_data[ele]}" if coll_data[ele]
+      end
     else
       puts("Same druids in #{first} and #{second} are released to #{sw_target}")
     end
   end
 end
 
-def collections_summary(argo_client, purl_client, sw_client, sw_target)
+def collections_summary(argo_client, purl_client, sw_client, coll_data, sw_target)
 
   argo_coll = argo_client.collections_druids
   pf_coll = purl_client.collections_druids
@@ -81,11 +84,11 @@ def collections_summary(argo_client, purl_client, sw_client, sw_target)
   puts("PURL has #{pf_coll.length} released to #{sw_target}")
   puts("SW has #{sw_coll.length} released to #{sw_target}")
 
-  rpt_output(differences(argo_coll, pf_coll, sw_coll), sw_target)
+  rpt_output(differences(argo_coll, pf_coll, sw_coll), coll_data, sw_target)
 
 end
 
-def individual_items_summary(argo_client, purl_client, sw_client, sw_target)
+def individual_items_summary(argo_client, purl_client, sw_client, coll_data, sw_target)
 
   argo_items = argo_client.items_druids
   pf_items = purl_client.items_druids
@@ -100,7 +103,7 @@ def individual_items_summary(argo_client, purl_client, sw_client, sw_target)
 
 end
 
-def individual_collection_summary(argo_client, purl_client, sw_client, sw_target, collection_druid)
+def individual_collection_summary(argo_client, purl_client, sw_client, coll_data, sw_target, collection_druid)
 
   fail "Must provide Environment variable COLL_DRUID with this script" if collection_druid.nil?
   argo_mem = argo_client.collection_members(collection_druid)
@@ -116,7 +119,7 @@ def individual_collection_summary(argo_client, purl_client, sw_client, sw_target
 
 end
 
-def everything_released_summary(argo_client, purl_client, sw_client, sw_target)
+def everything_released_summary(argo_client, purl_client, sw_client, coll_data, sw_target)
   argo_all = argo_client.all_druids
   pf_all = purl_client.all_druids
   sw_all = sw_client.all_druids
@@ -132,13 +135,13 @@ end
 
 case report_type
 when "Collections Summary"
-  collections_summary(argo_client, purl_client, sw_client, sw_target)
+  collections_summary(argo_client, purl_client, sw_client, coll_data, sw_target)
 when "Individual Items Summary"
-  individual_items_summary(argo_client, purl_client, sw_client, sw_target)
+  individual_items_summary(argo_client, purl_client, sw_client, coll_data, sw_target)
 when "Collection-specific Summary"
-  individual_collection_summary(argo_client, purl_client, sw_client, sw_target, collection_druid)
+  individual_collection_summary(argo_client, purl_client, sw_client, coll_data, sw_target, collection_druid)
 when "Everything Released Summary"
-  everything_released_summary(argo_client, purl_client, sw_client, sw_target)
+  everything_released_summary(argo_client, purl_client, sw_client, coll_data, sw_target)
 else
-  collections_summary(argo_client, purl_client, sw_client, sw_target)
+  collections_summary(argo_client, purl_client, sw_client, coll_data, sw_target)
 end
