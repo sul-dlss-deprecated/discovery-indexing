c564ae7dbd79d184c0878e53e5bb32bceff7dca5
Laney McGlohon <laneymcg@stanford.edu>
2017-01-18 14:01:58 -0800

    Add .gitignore

--- audit_script.sh
+++
@@ -1,9 +0,0 @@
-require "./audit.rb"
-argo_url="https://sul-solr.stanford.edu/solr/argo3_prod"
-pf_url="https://purl-fetcher-prod.stanford.edu"
-sw_url="http://searchworks-solr-lb:8983/solr/current"
-sw_tgt="searchworks"
-rpt_type="Collection Summary"
-
-a=Audit.new(argo_url, pf_url, sw_url, sw_tgt, rpt_type)
-a.rpt_select()
